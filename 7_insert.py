13ea3ffe41e4a907b4f620905bcabca1d6df65c8
coffeemori <coffeemori92@naver.com>
2022-04-16 20:03:58 +0900

    엑셀 행 삽입 삭제 이동

---
+++ 7_insert.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+
+wb = load_workbook('sample.xlsx')
+ws = wb.active
+
+# ws.insert_rows(8)
+# ws.insert_rows(8, 5)
+# ws.insert_cols(2)
+
+# wb.save('sample_insert_rows.xlsx')
+
+ws.insert_cols(2, 3)
+
+wb.save('sample_insert_cols.xlsx')
